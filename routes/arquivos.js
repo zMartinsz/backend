29110c7ee548713ec3ea8240e081f8e570292dd8
zMartinsz <maugusto22112008@gmail.com>
2025-09-24 16:57:09 -0300

    v8.0.2

--- routes/arquivos.js
+++ routes/arquivos.js
@@ -47,7 +47,7 @@ router.post('/upload', upload.single('arquivo'), async (req, res) => {
 //#endregion
 
 //#region download
-router.post('/download', auth, async (req, res) => {
+router.post('/download', async (req, res) => {
   try{const arquivo = await Arquivo.findById(req.body.id);
   if (!arquivo) return res.status(404).json({ message: 'Arquivo não encontrado' });
 
