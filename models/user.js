b5c2de22c07eaa32f6b6485278e4dc285bc421f2
Augusto Martins <133165302+zMartinsz@users.noreply.github.com>
2025-11-06 14:53:15 -0300

    Replace email field with cpf in user schema

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 
 const userschema = new mongoose.Schema({
     name: { type: String, trim: true, required: false },
-    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    cpf: { type: String, required: true, unique: true, lowercase: true, trim: true },
     password: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
     type: {type: [String], default: ["motorista_caminhao"], required: true, lowercase: true, trim: true},
