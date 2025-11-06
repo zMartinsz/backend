bff69f4ef4293c4d4dd6df1d42fe96472486cf64
Augusto Martins <133165302+zMartinsz@users.noreply.github.com>
2025-11-06 14:32:14 -0300

    v11

--- utils/valid.js
+++ utils/valid.js
@@ -3,6 +3,24 @@ function validEmail(email) {
   return re.test(email);
 }
 
+function validCPF(cpf) {
+  cpf = cpf.replace(/[^\d]+/g, ''); // remove pontuação
+  if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)) return false;
+
+  let soma = 0, resto;
+  for (let i = 1; i <= 9; i++) soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+  resto = (soma * 10) % 11;
+  if (resto === 10 || resto === 11) resto = 0;
+  if (resto !== parseInt(cpf.substring(9, 10))) return false;
+
+  soma = 0;
+  for (let i = 1; i <= 10; i++) soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+  resto = (soma * 10) % 11;
+  if (resto === 10 || resto === 11) resto = 0;
+  return resto === parseInt(cpf.substring(10, 11));
+}
+
+
 function validPassword(pw) {
   // Exemplo simples: >=6 chars. Ajusta se quiser mais segurança.
   return typeof pw === 'string' && pw.length >= 6;
@@ -22,4 +40,4 @@ function ValidDownload(role, tipo){
   return false;
 }
 
-module.exports = { validEmail, validPassword, ValidType, ValidEmpresa, ValidDownload };
+module.exports = { validEmail, validPassword, ValidType, ValidEmpresa, ValidDownload, ValidCPF };
